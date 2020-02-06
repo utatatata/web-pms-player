1ed5502767fbc1b042487de90d5471a4a7224415
utakuma <i@utakuma.info>
2020-02-06 21:55:40 +0900

    add routings using svelte-spa-routing

---
+++ src/routes/Home.svelte
@@ -0,0 +1,30 @@
+<script>
+  import {link} from 'svelte-spa-router'
+</script>
+
+<main>
+  <h1>Home</h1>
+  <a href="/importer" use:link>Importerへ</a>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ src/routes/Importer.svelte
@@ -0,0 +1,30 @@
+<script>
+  import {link} from 'svelte-spa-router'
+</script>
+
+<main>
+  <h1>Importer</h1>
+  <a href="/" use:link>Homeへ</a>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
